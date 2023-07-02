ff28732cd61400896143c10ff114d86ba04e685f
VelizaraSotirova <velizarasotirova@gmail.com>
2023-07-02 16:55:25 +0300

    Solutions for Streams, Files and Directories

---
+++ 04.StreamsFilesDirectories/Exercise/DirectoryTraversal/DirectoryTraversal.cs
@@ -0,0 +1,57 @@
+﻿namespace DirectoryTraversal
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class DirectoryTraversal
+    {
+        static void Main()
+        {
+            string path = Console.ReadLine();
+            string reportFileName = @"\report.txt";
+
+            string reportContent = TraverseDirectory(path);
+            Console.WriteLine(reportContent);
+
+            WriteReportToDesktop(reportContent, reportFileName);
+        }
+
+        public static string TraverseDirectory(string inputFolderPath)
+        {
+            SortedDictionary<string, List<FileInfo>> extensionFiles = new SortedDictionary<string, List<FileInfo>>();
+            string[] files = Directory.GetFiles(inputFolderPath);
+
+            foreach (var file in files)
+            {
+                FileInfo fileInfo = new FileInfo(file);
+
+                if (!extensionFiles.ContainsKey(fileInfo.Extension))
+                {
+                    extensionFiles[fileInfo.Extension].Add(fileInfo);
+                }
+            }
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in extensionFiles.OrderByDescending(exf => exf.Value.Count))
+            {
+                sb.AppendLine(item.Key);
+
+                foreach (var file in item.Value.OrderBy(f => f.Length))
+                {
+                    sb.AppendLine($"--{file.Name} - {(double)file.Length/1024:f3}kb");
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        public static void WriteReportToDesktop(string textContent, string reportFileName)
+        {
+            string filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + reportFileName;
+            File.WriteAllText(filePath, textContent);
+        }
+    }
+}
---
+++ 04.StreamsFilesDirectories/Exercise/EvenLines/EvenLines.cs
@@ -0,0 +1,63 @@
+﻿namespace EvenLines
+{
+    using System;
+    using System.IO;
+    using System.Linq;
+    using System.Text;
+
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                string line = string.Empty;
+                int count = 0;
+
+                StringBuilder sb = new StringBuilder();
+                while (!reader.EndOfStream)
+                {
+                    line = reader.ReadLine();
+                    if (count % 2 == 0)
+                    {
+                        string replacedSymbols = ReplaceSymbols(line);
+                        string reversedWords = ReverseWords(replacedSymbols);
+                        sb.Append(reversedWords);
+                    }
+                    count++;
+                }
+
+                return sb.ToString();
+            }
+        }
+
+        private static string ReplaceSymbols(string text)
+        {
+            StringBuilder sb = new StringBuilder(text);
+            char[] symbolsToReplace = { '-', ',', '.', '!', '?' };
+            foreach(char symbol in symbolsToReplace)
+            {
+                sb = sb.Replace(symbol, '@');
+            }
+
+            return sb.ToString();
+        }
+
+        private static string ReverseWords(string words)
+        {
+            string[] reversedWords = words
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Reverse()
+                .ToArray();
+
+            return string.Join(" ", reversedWords);
+        }
+    }
+}
