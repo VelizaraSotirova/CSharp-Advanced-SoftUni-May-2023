94855a2183eedb1c36380d8eb0810bfb351ed85a
VelizaraSotirova <velizarasotirova@gmail.com>
2023-07-09 10:27:50 +0300

    Solutions for Generics Lab

---
+++ 07.Generics/Lab/T01.Box/Box.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> items;
+
+        public Box()
+        { 
+            items = new List<T>();
+        }
+        public void Add(T element)
+        {
+            items.Add(element);
+        }
+        public T Remove()
+        {
+            T firstElement = items[this.items.Count-1];
+            items.RemoveAt(this.items.Count - 1 );
+            return firstElement;
+        }
+
+        public int Count
+        {
+            get { return this.items.Count; }
+        }
+    }
+}
---
+++ 07.Generics/Lab/T02.ArrayCreator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = ArrayCreator.Create(50, 3);
+            Console.WriteLine(String.Join(", ", numbers));
+        }
+    }
+}
